f571d5a7467a60d97237ca9348c0d316a4c4788e
jtrobec <3035875+jtrobec@users.noreply.github.com>
2021-03-21 15:11:57 -0500

    Update: .DS_Store Assignments LICENSE README.md notebooks

---
+++ Assignments/final_project_team25_centrality.py
@@ -0,0 +1,92 @@
+# Databricks notebook source
+# MAGIC %md # w261 Final Project - Clickthrough Rate Prediction
+
+# COMMAND ----------
+
+# MAGIC %md 25   
+# MAGIC Justin Trobec, Jeff Li, Sonya Chen, Karthik Srinivasan
+# MAGIC Spring 2021, section 5, Team 25
+
+# COMMAND ----------
+
+# MAGIC %md ## Table of Contents
+# MAGIC 
+# MAGIC * __Section 1__ - Question Formulation
+# MAGIC * __Section 2__ - Algorithm Explanation
+# MAGIC * __Section 3__ - EDA & Challenges
+# MAGIC * __Section 4__ - Algorithm Implementation
+# MAGIC * __Section 5__ - Course Concepts
+
+# COMMAND ----------
+
+# MAGIC %md # __Section 1__ - Question Formulation
+
+# COMMAND ----------
+
+# MAGIC %md 
+
+# COMMAND ----------
+
+# MAGIC %md 
+
+# COMMAND ----------
+
+# MAGIC %md # __Section 2__ - Algorithm Explanation
+
+# COMMAND ----------
+
+# MAGIC %md 
+
+# COMMAND ----------
+
+# MAGIC %md 
+
+# COMMAND ----------
+
+# MAGIC %md # __Section 3__ - EDA & Challenges
+
+# COMMAND ----------
+
+# MAGIC %md ### Load Spark GraphX libraries.
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC 
+# MAGIC import org.apache.spark._
+# MAGIC import org.apache.spark.graphx._
+# MAGIC import org.apache.spark.rdd.RDD
+# MAGIC 
+# MAGIC import org.apache.spark.graphx.GraphLoader
+# MAGIC import org.apache.spark.graphx.GraphOps
+
+# COMMAND ----------
+
+# MAGIC %md 
+
+# COMMAND ----------
+
+# MAGIC %md # __Section 4__ - Algorithm Implementation
+
+# COMMAND ----------
+
+# MAGIC %md 
+
+# COMMAND ----------
+
+# MAGIC %md 
+
+# COMMAND ----------
+
+# MAGIC %md # __Section 5__ - Course Concepts
+
+# COMMAND ----------
+
+# MAGIC %md 
+
+# COMMAND ----------
+
+# MAGIC %md 
+
+# COMMAND ----------
+
